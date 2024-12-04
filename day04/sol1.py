ca6955cfec475740242f5c38e2b9797bc9315be8
MF <mfitto@gmail.com>
2024-12-04 07:35:50 +0100

    day04

--- day04/sol1.py
+++ day04/sol1.py
@@ -1,19 +1,139 @@
 import re
-from typing import List
+from typing import List, Dict
 
 
-def read_file(file_name: str):
-    txt = ''
+def read_file(file_name: str) -> List[List[str]]:
+    text_table = []
 
-    pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    pattern = r'XMAS'
 
     with open(file_name, "r") as file:
         for line in file:
-            print(line.rstrip())
-            txt = re.sub(pattern, r'\1,\2', line)
+            row = []
+            for char in line.rstrip():
+                row += char
 
-    return txt
+            # print(row)
+            text_table.append(row)
+
+    return text_table
 
 
-x = read_file('data/test1.txt')
-print(x)
+def prepare_all_tables(text_table: List[List[str]]) -> Dict[str, List[List[str]]]:
+    # horizontal,
+    outputs = {'horizontal': text_table.copy()}
+
+    # # horizontal - written backwards
+    # hb = [row[::-1] for row in text_table]
+    # print(hb)
+
+    # vertical
+    vertical = [[row[i] for row in text_table] for i in range(len(text_table))]
+    # print(vertical)
+    outputs['vertical'] = vertical
+
+    # diagonal
+    print('0')
+    diagonals = []
+    for i in range(len(text_table)):
+        word = []
+        x = i + 1
+        y = 0
+        while y < len(text_table[0]) and x < len(text_table):
+            word.append(text_table[x][y])
+            x += 1
+            y += 1
+
+        print(word)
+        diagonals.append(word)
+    # print(diagonals)
+
+    print('1')
+    for i in range(len(text_table)):
+        word = []
+        x = len(text_table) - 2 - i
+        y = 0
+        while y < len(text_table[0]) and x > -1:
+            word.append(text_table[x][y])
+            x -= 1
+            y += 1
+
+        print(word)
+        diagonals.append(word)
+
+    print('2')
+    for i in range(len(text_table[0])):
+        word = []
+        x = 0
+        y = i
+        while y < len(text_table[0]) and x < len(text_table):
+            word.append(text_table[x][y])
+            x += 1
+            y += 1
+
+        print(word)
+        diagonals.append(word)
+
+    print('3')
+    for i in range(len(text_table[0])):
+        word = []
+        x = len(text_table) - 1
+        y = i
+        while y < len(text_table[0]) and x > -1:
+            word.append(text_table[x][y])
+            x -= 1
+            y += 1
+
+        print(word)
+        diagonals.append(word)
+
+    outputs['diagonal'] = diagonals
+
+    return outputs
+
+
+def find_strings(text_tables: Dict[str, List[List[str]]],
+                 pattern_strings: List[str]
+                 ) -> int:
+    instances = 0
+    for pattern in pattern_strings:
+        for key, table in text_tables.items():
+            for line in table:
+                text_line = ''.join(line)
+                matches = re.findall(pattern, text_line)
+                instances += len(matches)
+
+                print(f'-----------------------')
+                print(f'key {key}')
+                print(f'pattern {pattern}')
+                print(f'text_line {text_line}')
+                print(f'matches {len(matches)}')
+
+    return instances
+
+
+# matrix = [
+#     ['t', 'e', 's', 't', 'a'],
+#     ['u', 'p', 't', 'o', 'a'],
+#     ['d', 'i', 'a', 'g', 'a'],
+#     ['t', 'a', 'b', 'l', 'a'],
+#     ['e', 'x', 't', 'r', 'a']
+# ]
+# x = prepare_all_tables(matrix)
+# for x1 in x.keys():
+#     print(f'{x1} : {x[x1]}')
+
+# x = read_file('data/test1.txt')
+x = read_file('data/task1.txt')
+# print(x)
+y = prepare_all_tables(x)
+# for x1 in y.keys():
+#     print(f'{x1} : {y[x1]}')
+
+# print(y)
+
+z = find_strings(y,
+                 ['XMAS', 'SAMX']
+                 )
+print(z)
+
---
+++ day04/sol2.py
@@ -0,0 +1,70 @@
+import re
+from typing import List, Dict
+
+
+def read_file(file_name: str) -> List[List[str]]:
+    text_table = []
+
+    pattern = r'XMAS'
+
+    with open(file_name, "r") as file:
+        for line in file:
+            row = []
+            for char in line.rstrip():
+                row += char
+
+            # print(row)
+            text_table.append(row)
+
+    return text_table
+
+
+def find_strings(text_table: List[List[str]],
+                 ) -> int:
+    instances = 0
+    for x in range(len(text_table) - 2):
+        for y in range(len(text_table[0]) - 2):
+            if text_table[x][y] == 'M' and text_table[x][y + 2] == 'S' and text_table[x + 1][y + 1] == 'A' and \
+                    text_table[x + 2][y] == 'M' and text_table[x + 2][y + 2] == 'S':
+                print(x)
+                print(y)
+                print('-----------')
+                instances += 1
+            if text_table[x][y] == 'S' and text_table[x][y + 2] == 'S' and text_table[x + 1][y + 1] == 'A' and \
+                    text_table[x + 2][y] == 'M' and text_table[x + 2][y + 2] == 'M':
+                print(x)
+                print(y)
+                print('-----------')
+                instances += 1
+            if text_table[x][y] == 'S' and text_table[x][y + 2] == 'M' and text_table[x + 1][y + 1] == 'A' and \
+                    text_table[x + 2][y] == 'S' and text_table[x + 2][y + 2] == 'M':
+                print(x)
+                print(y)
+                print('-----------')
+                instances += 1
+            if text_table[x][y] == 'M' and text_table[x][y + 2] == 'M' and text_table[x + 1][y + 1] == 'A' and \
+                    text_table[x + 2][y] == 'S' and text_table[x + 2][y + 2] == 'S':
+                print(x)
+                print(y)
+                print('-----------')
+                instances += 1
+
+    return instances
+
+
+# matrix = [
+#     ['t', 'e', 's', 't', 'a'],
+#     ['u', 'p', 't', 'o', 'a'],
+#     ['d', 'i', 'a', 'g', 'a'],
+#     ['t', 'a', 'b', 'l', 'a'],
+#     ['e', 'x', 't', 'r', 'a']
+# ]
+# x = prepare_all_tables(matrix)
+# for x1 in x.keys():
+#     print(f'{x1} : {x[x1]}')
+
+# x = read_file('data/test1.txt')
+x = read_file('data/task1.txt')
+
+z = find_strings(x)
+print(z)
